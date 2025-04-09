60a60aeb9aa0820f7fab5d315319db667315278b
pond-e <haruto2266@icloud.com>
2025-04-09 22:16:47 +0900

    add WorksPage and Link

---
+++ src/pages/WorksPage.tsx
@@ -0,0 +1,32 @@
+function WorksPage() {
+    return (
+        
+        <div className="max-w-md mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">作成物一覧</h1>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden mb-4">
+            <div className="bg-gray-300 h-48"></div>
+            <div className="p-4">
+                <h2 className="text-xl font-bold">BgGapFiller</h2>
+                <p className="text-gray-600">背景画像生成 Web アプリ</p>
+            </div>
+        </div>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden mb-4">
+            <div className="bg-gray-300 h-48"></div>
+            <div className="p-4">
+                <h2 className="text-xl font-bold">BgGapFiller</h2>
+                <p className="text-gray-600">背景画像生成 Web アプリ</p>
+            </div>
+        </div>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden mb-4">
+            <div className="bg-gray-300 h-48"></div>
+            <div className="p-4">
+                <h2 className="text-xl font-bold">BgGapFiller</h2>
+                <p className="text-gray-600">背景画像生成 Web アプリ</p>
+            </div>
+        </div>
+    </div>
+    
+    );
+}
+
+export default WorksPage;
