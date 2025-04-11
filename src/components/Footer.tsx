3efecbdba7393e119b6fe267466bf36c9642de0e
pond-e <haruto2266@icloud.com>
2025-04-11 22:38:28 +0900

    adjust font size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ function Footer() {
     return (
         <>
             <footer className="mt-auto">
-                <div className="bg-stone-900 pt-12 mt-10 text-white text-center text-3xl">
+                <div className="bg-stone-900 pt-12 mt-10 text-white text-center text-2xl">
                     <small>&copy; 2025 pond</small>
-                    <a className="ml-8 text-2xl" href="https://x.com/picuto0206">X</a>
-                    <a className="ml-8 text-2xl" href="https://github.com/pond-e">GitHub</a>
+                    <a className="ml-8 text-xl" href="https://x.com/picuto0206">X</a>
+                    <a className="ml-8 text-xl" href="https://github.com/pond-e">GitHub</a>
                 </div>
             </footer>
         </>
